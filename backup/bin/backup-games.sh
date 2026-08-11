1fceaa062c995344b238bdd0b53742e102184602
Felina Rivera Calzadillas (ROGUH) <felina.rivera@powerflex.com>
2026-08-11 15:59:55 -0600

    zed + improved game backups

--- backup/bin/backup-games.sh
+++ backup/bin/backup-games.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
+ROOT="$(get-backup-root.sh)/$(date '+%Y-%m')"
+PREV="$(get-backup-root.sh)/$(date -d "$(date '+%Y-%m') - 15 last month" '+%Y-%m')"
 
-ROOT=$(get-backup-root.sh)/$(date +%Y-%m)
-
-mkdir -p $ROOT/game-backups
+mkdir -p "$ROOT/game-backups"
 
 # delete extraneous files from destination
 RSYNC_OPTS="--archive --human-readable --delete-after --recursive --ignore-missing-args"
 
-echo BACKUP WEBFISHING
-# $HOME/.steam/steam/steamapps/compatdata/3146520/pfx/drive_c/users/steamuser/AppData/Roaming/Godot/app_userdata/webfishing_2_newver
-WEBFISH="$HOME/.steam/steam/steamapps/compatdata/3146520/pfx/drive_c/users/steamuser/AppData/Roaming" 
-rsync $RSYNC_OPTS \
-  "$WEBFISH" $ROOT/game-backups/webfishing
+_backup_game() {
+  SRC="$1"
+  DEST="$2"
+  NAME="$3"
+  PUBLISHED_YEAR="$4"
+  if [ -d "$SRC" ] && ! diff -qr "$SRC" "$PREV/$DEST" ; then
+    echo "Game backup: $NAME ($PUBLISHED_YEAR) from $SRC"
+    rsync $RSYNC_OPTS "$SRC/" "$ROOT/$DEST"
+  fi
+}
 
-echo BACKUP DARK SOULS II
-DS2="$HOME/.steam/steam/steamapps/compatdata/335300/pfx/drive_c/users/steamuser/AppData/Roaming"
-rsync $RSYNC_OPTS \
-  "$DS2" $ROOT/game-backups/DS2_dark_souls_ii_DarkSoulsII
+_backup_game \
+  "$HOME/.steam/steam/steamapps/compatdata/3146520/pfx/drive_c/users/steamuser/AppData/Roaming"  \
+  "game-backups/webfishing" \
+  "WEBFISHING" "2024"
 
-if [ -f "$HOME/.steam/steam/steamapps/common/Call of Duty\ 2/main/players/" ] ; then
-echo BACKUP CALL OF DUTY 2 2005
-COD2="$HOME/.steam/steam/steamapps/common/Call of Duty\ 2/main/players"
-rsync $RSYNC_OPTS \
-  "$COD2" $ROOT/game-backups/call_of_duty_2_2005
-fi
+_backup_game \
+  "$HOME/.steam/steam/steamapps/compatdata/335300/pfx/drive_c/users/steamuser/AppData/Roaming" \
+  "game-backups/DS2_dark_souls_ii_DarkSoulsII" \
+  "DARK SOULS II" "2011"
 
-LILKIT="$HOME/.steam/steam/steamapps/compatdata/1177980/pfx/drive_c/users/steamuser/AppData/LocalLow"
-if [ -d "$LILKIT" ] ; then
-echo BACKUP LKBC 2005
-rsync $RSYNC_OPTS \
-  "$LILKIT" $ROOT/game-backups/little_kitten_big_city
-fi
+_backup_game \
+  "$HOME/.steam/steam/steamapps/common/Call of Duty\ 2/main/players" \
+  "game-backups/call_of_duty_2_2005" \
+  "CALL OF DUTY 2" "2005"
 
-VTMB="$HOME/Games/Heroic/VtMB/Unofficial_Patch/save/"
-if [ -d "$LILKIT" ] ; then
-echo "BACKUP Vampire the Masquerade Bloodlines (2004)"
-rsync $RSYNC_OPTS \
-  "$VTMB" $ROOT/game-backups/vtmb_2004
-fi
+_backup_game \
+  "$HOME/.steam/steam/steamapps/compatdata/1177980/pfx/drive_c/users/steamuser/AppData/LocalLow" \
+  "game-backups/little_kitten_big_city" \
+  "LKBC" "2024"
+
+_backup_game \
+  "$HOME/Games/Heroic/VtMB/Unofficial_Patch/save/" \
+  "game-backups/vtmb_2004" \
+  "Vampire the Masquerade Bloodlines" "2004"
