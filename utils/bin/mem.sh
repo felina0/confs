2660d2077642c77c3332c024a53dc495a58944e5
Felina Rivera Calzadillas (ROGUH) <felina.rivera@powerflex.com>
2026-08-20 09:34:44 -0600

    mem.sh

--- utils/bin/mem.sh
+++ utils/bin/mem.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 free | grep 'Mem:' | awk '{printf("%.1fG RAM", ($3 + $5) / 1000000)}'
-printf ' %s STO' "$(df -h / | tail -n1 | cut -d' ' -f9)"
+# printf ' %s STO' "$(df -h / | tail -n1 | cut -d' ' -f9)"
+# printf ' %s CLD' "$(claude --print '/usage' | grep 'Current session' | grep '[0-9]*%' --only-matching)"
